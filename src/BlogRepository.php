9cd4d70c9ae2ebdc29fe7daff87403821a1707fa
Rick Strafy <rixafy@gmail.com>
2019-02-28 22:35:53 +0100

    Add basic methods to BlogRepository

--- src/BlogRepository.php
+++ src/BlogRepository.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace Rixafy\Blog;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
+use Ramsey\Uuid\Uuid;
 
 class BlogRepository
 {
@@ -14,5 +17,36 @@ class BlogRepository
     public function __construct(EntityManagerInterface $entityManager)
     {
         $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @return EntityRepository|\Doctrine\Common\Persistence\ObjectRepository
+     */
+    protected function getRepository()
+    {
+        return $this->entityManager->getRepository(Blog::class);
+    }
+
+    /**
+     * @param string $id
+     * @return Blog|null
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function find(string $id): ?Blog
+    {
+        return $this->getQueryBuilderForAll()
+            ->andWhere('b.id = :id')->setParameter('id', Uuid::fromString($id))
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
+
+    /**
+     * @return QueryBuilder
+     */
+    public function getQueryBuilderForAll(): QueryBuilder
+    {
+        return $this->getRepository()->createQueryBuilder('b')
+            ->where('b.is_active = :active')->setParameter('active', true)
+            ->orderBy('b.created_at');
     }
 }
