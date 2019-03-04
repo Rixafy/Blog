d0a65414970119fb41a45865bc25a308a2491fd3
Rick Strafy <rixafy@gmail.com>
2019-03-04 23:01:37 +0100

    Add BlogPublisher DTO

---
+++ src/BlogPublisher/BlogPublisherData.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rixafy\Blog\BlogPublisher;
+
+use Rixafy\Blog\Blog;
+
+class BlogPublisherData
+{
+    /** @var string */
+    public $displayName;
+
+    /** @var string */
+    public $signature;
+
+    /** @var Blog */
+    public $blog;
+}
